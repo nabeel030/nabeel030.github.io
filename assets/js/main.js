545315ea978e653238ccb4a6b04b13a714616c94
nabeel030 <nabeelahmed030@gmail.com>
2022-12-13 23:34:55 +0500

    Add new testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,7 +140,7 @@
 
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
+    autoplay: false,
     dots: true,
     loop: true,
     responsive: {
